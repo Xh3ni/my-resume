f6b8b375ceb123f3c74c39f2324eedaf3ae0daf8
Xh3ni <xhensilastambolliu@yahoo.com>
2023-03-05 22:04:50 +0000

    Add email

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -20,7 +20,7 @@ function repoInformationHTML(repos) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
-    var listItemsHTML = repos.map(function(repo) {
+    var listItemsHTML = repos.map(function (repo) {
         return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
@@ -66,6 +66,10 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(
                     `<h2>No info found for the user ${username}</h2>`
                 );
+            } else if (errorResponse === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
